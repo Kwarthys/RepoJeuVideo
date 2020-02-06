c56edbea8ee01b18e8673a63c03679d8fcc60a9b
Kwarthys <Kwarthys@gmail.com>
2020-02-06 18:20:56 +0100

    first interaction tests

---
+++ TheGame/Assets/Scripts/RayCaster.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayCaster : MonoBehaviour
+{
+    private GameObject raycastedObject;
+
+    [SerializeField] private int rayLenght = 3;
+    [SerializeField] private LayerMask layerMaskInteract;
+
+    [SerializeField] private Image uiCrossHair;
+
+    private bool crossHairActivated = false;
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if(Physics.Raycast(transform.position, fwd, out hit, rayLenght, layerMaskInteract.value))
+        {
+            if(hit.collider.CompareTag("Object"))
+            {
+                raycastedObject = hit.collider.gameObject;
+
+                if(!crossHairActivated)
+                {
+                    crossHairActive(true);
+                }
+                        
+
+                if (Input.GetKeyDown("e"))
+                {
+                    Debug.Log("yee");
+                    //Dosmthg
+                }
+            }
+        }
+        else
+        {
+            if(crossHairActivated)
+            {
+                crossHairActive(false);
+            }
+        }
+
+    }
+
+    void crossHairActive(bool status)
+    {
+        crossHairActivated = status;
+        uiCrossHair.color = status ? Color.red : Color.white;
+    }
+
+}
