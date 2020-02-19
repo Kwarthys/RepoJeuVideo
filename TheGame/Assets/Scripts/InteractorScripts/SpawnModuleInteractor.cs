2fd8395079520d93013efa0883b720f77aad11b5
Kwarthys <Kwarthys@gmail.com>
2020-02-19 17:03:31 +0100

    First iteration of module spawning and Refinery module

---
+++ TheGame/Assets/Scripts/InteractorScripts/SpawnModuleInteractor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnModuleInteractor : AbstractInteract
+{
+    public GameObject modulePrefab;
+
+    private Transform spawnPoint;
+
+    public void Start()
+    {
+        spawnPoint = transform.Find("SpawnPoint");
+    }
+
+    public override void execute()
+    {
+        gameObject.SetActive(false);
+
+        Instantiate(modulePrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
