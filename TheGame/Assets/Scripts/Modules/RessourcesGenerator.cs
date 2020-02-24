e40ed3672340c25c5d7d8bb87e88bfd6b58bf541
Kwarthys <Kwarthys@gmail.com>
2020-02-24 20:39:59 +0100

    Hangar now spawning crates !

--- TheGame/Assets/Scripts/Modules/RessourcesGenerator.cs
+++ TheGame/Assets/Scripts/Modules/RessourcesGenerator.cs
@@ -6,24 +6,44 @@ public class RessourcesGenerator : MonoBehaviour
 {
     private RessourceManager rManager;
 
+    public GameObject cratePrefab;
     public RessourceManager.Ressources generatedResource;
     public int timeToProcess;
     public int generatedAmount;
+
+    public Transform holderAndRef;
 
     private int processIndex = 0;
 
     void Start()
     {
         rManager = GameObject.FindWithTag("RessourceManager").GetComponent<RessourceManager>();
+        //cratePrefab.GetComponent<CrateBehavior>().r = generatedResource;
     }
 
 
     void Update()
     {
-        if (++processIndex >= timeToProcess)
+        if(holderAndRef.childCount == 0)
         {
-            rManager.giveRessources(generatedResource, generatedAmount);
-            processIndex = 0;
+            if (++processIndex >= timeToProcess)
+            {
+                //SPAWN
+                Vector3 offset = new Vector3(0, 0.75f, 0);
+                for(int i = 0; i < generatedAmount; ++i)
+                {
+                    spawnCrate(offset);
+                    offset += 1.2f * holderAndRef.forward;
+                }
+                processIndex = 0;
+            }
         }
+    }
+
+    private void spawnCrate(Vector3 offset)
+    {
+        GameObject g = Instantiate(cratePrefab, holderAndRef.position + offset, Quaternion.identity, holderAndRef);
+        g.GetComponent<CrateBehavior>().r = generatedResource;
+        Debug.Log("wanted to spawn " + generatedResource);
     }
 }
